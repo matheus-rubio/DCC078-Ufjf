9ecc25c1a4bf6ef3c5e69c765dcc28fe4e0562a5
Matheus Rubio <matheus.rubio@ice.ufjf.br>
2023-09-14 19:51:13 -0300

    Init projects

---
+++ factory-method/src/main/java/com/dcc/ufjf/App.java
@@ -0,0 +1,13 @@
+package com.dcc.ufjf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
